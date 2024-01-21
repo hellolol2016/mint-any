826f10d580b4fd7d44e5996709b3a018cc08e4e0
Dennis Wang <denniswang812@gmail.com>
2024-01-21 12:52:25 +0000

    two links

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -53,6 +53,7 @@ function go() {
     router.push(`/hi/${encodeURIComponent(name.value)}`)
 }
 
+const contractAddress = 'p0x8163647d1108E2dfF08d17CfE866822CCb45bD2B'
 // Function that loads the image from the backend
 // TO-DO
 </script>
@@ -126,6 +127,21 @@ function go() {
           <img v-if="MintSuccessful" :src="displayImage" alt="Display Image">
         </div>
       </div>
+              <div>
+          View the
+          <a href={jsonUrl} target="_blank" rel="noopener noreferrer">
+            Metadata
+          </a>
+        </div>
+        <div>
+          <a
+            href="https://sepolia.etherscan.io/address/0x8163647d1108E2dfF08d17CfE866822CCb45bD2B"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            View in Etherscan
+          </a>
+        </div>
     </div>
   </div>
 </template>
